6ba04120bf20a0966134c0d212bf62ee58f79daf
jdoubleu <hi@jdoubleu.de>
2024-03-10 21:03:33 +0100

    isolate issue

--- main.cpp
+++ main.cpp
@@ -1,24 +1,33 @@
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#include <Windows.h>
-#include <combaseapi.h>
-
-#include "Base.hpp"
-
-// {2A82E6CD-6E11-45F0-B61C-D77592F776B5}
-static const GUID IID_ITarget = { 0x2a82e6cd, 0x6e11, 0x45f0, { 0xb6, 0x1c, 0xd7, 0x75, 0x92, 0xf7, 0x76, 0xb5 } };
-
-struct __declspec(uuid("2A82E6CD-6E11-45F0-B61C-D77592F776B5")) ITarget : IUnknown
+struct ITarget
 {
+    static constexpr int ID = 42;
 };
 
-struct Implementation : Base<ITarget>
+template<typename... InterfaceT>
+struct Factory : public InterfaceT...
+{
+	void create(int id, void** ppvObject)
+	{
+		*ppvObject = ([&] {
+			void* inner = nullptr;
+
+			(([&]<typename T>() {
+				if (T::ID == id)
+					inner = this;
+			}).template operator()<InterfaceT>(), ...);
+
+			return inner;
+		})();
+	}
+};
+
+struct Implementation : Factory<ITarget>
 {
 };
 
 int main()
 {
     void* object;
-    Implementation{}.QueryInterface(IID_ITarget, &object);
+    Implementation{}.create(42, &object);
     delete object;
 }
